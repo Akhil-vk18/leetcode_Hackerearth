78358c8777276cad5d7bb04b072b3b018588137a
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-07-31 18:24:37 +0530

    Time: 0 ms (100%), Space: 45.5 MB (83.92%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,26 +2,35 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
-        HashMap<Integer, int[]> zeros = new HashMap<>();
-        int zero_i = 0;
-        ;
-        int[] pos = new int[2];
+        boolean row = false;
+        boolean col = false;
+
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < cols; j++) {
                 if (matrix[i][j] == 0) {
-                 
-                    zeros.put(++zero_i, new int[]{i,j});
+                    if (i == 0)
+                        row = true;
+                    if (j == 0)
+                        col = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
 
-        for (Integer key : zeros.keySet()) {
-            pos = zeros.get(key);
-            for (int i = 0; i < rows; i++) {
-                matrix[i][pos[1]] = 0;
+        for (int i = 1; i < rows; i++)
+            for (int j = 1; j < cols; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
             }
-            for (int i = 0; i < cols; i++) {
-                matrix[pos[0]][i] = 0;
-            }
+
+        if(row){
+            for(int i=0;i<cols;i++)
+                matrix[0][i]=0;
+        }
+          if(col){
+            for(int i=0;i<rows;i++)
+                matrix[i][0]=0;
         }
     }
 }
