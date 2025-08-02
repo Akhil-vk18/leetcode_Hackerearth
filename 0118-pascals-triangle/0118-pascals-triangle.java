9b87f4cba0c27d3e355b08553be71b669054741a
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-08-02 12:40:32 +0530

    Time: 1 ms (82.52%), Space: 41.7 MB (98.49%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -6,17 +6,15 @@ class Solution {
         if(numRows==1){
         return res;
         }
-        int index=0;
-        
         for(int i =1;i<=numRows-1;i++){
-            index=0;
+           
              List<Integer> prevlist =res.get(i-1);
             List<Integer> inlist =new  ArrayList<>();
             inlist.add(0,1);
           
             for(int j=1;j<=i-1;j++){
-                int value = prevlist.get(index) + prevlist.get(index+1);
-                inlist.add(j,value);index++;
+                int value = prevlist.get(j-1) + prevlist.get(j);
+                inlist.add(value);
             }
               inlist.add(1);
     res.add(inlist);
