ed376039bdcf0d24d6a4718cb79bbdcff2d1ebb6
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-08-06 20:37:16 +0530

    Time: 126 ms (31.24%), Space: 57.1 MB (5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,42 +1,28 @@
 class Solution {
     public String longestPalindrome(String s) {
+        int[][] length = new int[s.length()][s.length()];
         int n = s.length();
-        if (n <= 1) return s;
-
-        boolean[][] dp = new boolean[n][n];
-        int start = 0;
-        int maxLen = 1;
-
-        // 1. All single characters are palindromes
+        String result = "";
         for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
+            length[i][i] = 1;
+            result = s.substring(i, i + 1);
         }
-
-        // 2. Two-character palindromes
         for (int i = 0; i < n - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                start = i;
-                maxLen = 2;
+                length[i][i + 1] = 2;
+                result = s.substring(i, i + 2);
             }
         }
-
-        // 3. Check palindromes of length >= 3
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1; // end index
-
-                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    if (len > maxLen) {
-                        start = i;
-                        maxLen = len;
+        for (int len = 3; len <= n; len++)
+            for (int i = 0; len - 1 + i < n; i++) {
+                int j = len - 1 + i;
+                if (s.charAt(i) == s.charAt(j) && length[i + 1][j - 1] != 0) {
+                    length[i][j] = 2 + length[i + 1][j - 1];
+                    if (result.length() < length[i][j]) {
+                        result = s.substring(i, j + 1);
                     }
                 }
             }
-        }
-
-        // Return the longest palindrome substring
-        return s.substring(start, start + maxLen);
+        return result;
     }
 }
