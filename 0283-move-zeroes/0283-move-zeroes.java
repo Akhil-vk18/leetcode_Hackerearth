965bee183595b65fc6723958544de2848b75d8b4
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-08-08 10:42:54 +0530

    Time: 2 ms (79.83%), Space: 46.6 MB (12.6%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,13 +5,15 @@ class Solution {
         int nonzero= 0;
         for(int i=0;i<length;i++){
             if(nums[i]!=0){
+                int temp =nums[nonzero];
                 nums[nonzero++]=nums[i];
+                nums[i]=temp;
              
             }
         }
-        for(int i=nonzero;i<length;i++){
-            nums[i]=0;
-        }
+        // for(int i=nonzero;i<length;i++){
+        //     nums[i]=0;
+        // }
         
     }
 }
