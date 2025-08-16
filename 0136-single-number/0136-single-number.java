95c03779f4b9425d3b3bfbc19e6d94366843acba
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-08-17 00:00:22 +0530

    Time: 1 ms (99.84%), Space: 46.1 MB (60.51%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-         HashMap<Integer ,Integer> occurence =new HashMap<>();
-       for (int i : nums) {
-        occurence.put(i, occurence.getOrDefault(i,0)+1);
-       }
-       System.out.println(occurence);
-    for (int key : occurence.keySet()) {
-        if(occurence.get(key)==1){
-            return key;
-        }
-        
-    }
-    return-1;
+        int xor =00;
+      for (int i : nums) {
+        xor =xor^i;
+      }
+    return xor;
     }
 }
