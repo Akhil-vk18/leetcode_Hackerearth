8459cc0f4b55faeb47b73d2cc73b69092ed97e83
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-08-20 20:36:57 +0530

    Time: 4 ms (3.14%), Space: 43.7 MB (5.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,36 +1,38 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-      
-        int pivot =nums.length-2;
-        while (pivot >=0 && nums[pivot]>=nums[pivot+1]) {
+
+        int pivot = nums.length - 2;
+        while (pivot >= 0 && nums[pivot] >= nums[pivot + 1]) {
             pivot--;
         }
         //if pivot is -1 then all elements are in increasing order , so we need to return the loweest possinble order eg: for[3,2,1] we return [1,2,3]
-        if (pivot>=0) {
+        if (pivot >= 0) {
             //we need to find the first elment which is greater than the pivot element. then we swap them
-            int pi=nums.length-1;
-            while (pi>=0 && nums[pivot]>=nums[pi]) {
+            int pi = nums.length - 1;
+            while (pi >= 0 && nums[pivot] >= nums[pi]) {
                 pi--;
             }
             //swap the numbers
-            swap(pi, pivot ,nums);
+            swap(pi, pivot, nums);
         }
-        reverse(pivot+1, nums);
+        reverse(pivot + 1, nums);
         System.out.println(Arrays.toString(nums));
     }
 
-    public void swap(int pi,int pj,int[] nums){
-        
-            int temp=nums[pi];
-                nums[pi]=nums[pj];
-                nums[pj]=temp;
-                
-      
+    public void swap(int pi, int pj, int[] nums) {
+
+        int temp = nums[pi];
+        nums[pi] = nums[pj];
+        nums[pj] = temp;
+
     }
-    public void reverse(int start ,int[] nums){
-        int end=nums.length-1;
-        while (start<=end) {
-            swap(start, end, nums);start++;end--;
+
+    public void reverse(int start, int[] nums) {
+        int end = nums.length - 1;
+        while (start < end) {
+            swap(start, end, nums);
+            start++;
+            end--;
         }
     }
 }
