c7327f8c2eac43a23081cfbd99ed7a2d29089602
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-08-23 21:43:57 +0530

    Time: 106 ms (5.23%), Space: 55 MB (59.31%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int reversePairs(int[] nums) {
+
+        int length = nums.length;
+        int left = 0, right = length - 1;
+        int count = mergesort(left, right, nums);
+        System.out.println(Arrays.toString(nums));
+
+        return count;
+    }
+
+    public int mergesort(int low, int high, int[] nums) {
+        int count = 0;
+        if (low >= high) {
+            return count;
+        } else {
+            int mid = low + (high - low) / 2;
+            count = count + mergesort(low, mid, nums);
+            count = count + mergesort(mid + 1, high, nums);
+            count = count + countpairs(low, mid, high, nums);
+            merge(low, mid, high, nums);
+        }
+        return count;
+
+    }
+
+    public void merge(int low, int mid, int high, int[] nums) {
+        int left = low, right = mid + 1, i = low;
+        List<Integer> arr = new ArrayList<>();
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                arr.add(nums[left++]);
+            } else {
+                arr.add(nums[right++]);
+            }
+        }
+        while (left <= mid) {
+            arr.add(nums[left++]);
+        }
+        while (right <= high) {
+            arr.add(nums[right++]);
+        }
+
+        while (i <= high) {
+            nums[i] = arr.get(i - low);
+            i++;
+        }
+    }
+
+    public int countpairs(int low, int mid, int high, int[] nums) {
+        int right = mid + 1;
+        int count = 0;
+        for (int left = low; left <= mid; left++) {
+            while (right <= high && nums[left] > 2l * nums[right]) {
+                right++;
+            }
+            count += right - (mid + 1);
+        }
+        return count;
+    }
+}
