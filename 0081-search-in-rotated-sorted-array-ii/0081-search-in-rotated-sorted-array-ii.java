4c5eaae8c9dca6240f522ef53e163bfbecefe297
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-08-27 15:18:45 +0530

    Time: 14 ms (1.06%), Space: 44.7 MB (5.87%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    // 0(log n)
+    public boolean search(int[] nums, int target) {
+       return binarySearch(nums,target,0,nums.length-1);
+    }
+
+    private boolean binarySearch(int[] nums, int target,int low,int high) {
+        if (low > high ) {
+            return false;
+        }
+        int mid = low + (high - low) / 2;
+        System.out.println("low : " + low + " mid :" + mid+" high : " + high);
+        if (nums[mid] == target) {
+            return true;
+        }
+        //if values at low ,mid,high are same shrink the array
+        if (nums[low]==nums[mid]&&nums[mid]==nums[high]) {
+            return binarySearch(nums, target, low+1, high-1);
+        }
+        //left sorted 
+         if (nums[mid] >=nums[low]) {
+            if (target>=nums[low] && target<nums[mid]) {
+                return binarySearch(nums, target, low, mid - 1);
+            }else{
+                return binarySearch(nums, target, mid + 1, high);
+            }
+           
+        }
+        //right sorted
+         else {
+             if (target > nums[mid] && target <= nums[high]) {
+                 return binarySearch(nums, target, mid+1, high);
+             } else {
+                 return binarySearch(nums, target, low, mid-1);
+             }
+           
+        }
+       
+    }
+}
