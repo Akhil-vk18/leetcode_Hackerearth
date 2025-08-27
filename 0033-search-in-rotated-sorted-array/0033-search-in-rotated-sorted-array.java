0fbf8ce9b1bd0c7d83607771c19e526b4f08bfda
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-08-27 12:23:36 +0530

    Time: 0 ms (100%), Space: 42.2 MB (39.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,31 +1,36 @@
 class Solution {
+    // 0(log n)
     public int search(int[] nums, int target) {
-        int length = nums.length;
-        int i = length- 1;
-        while (i > 0 && nums[i - 1] < nums[i]) {
-            i--;
-        }
-        int index = binarySearch(nums, target, 0, i - 1);//lower bound
-        if (index == -1) {
-            index = binarySearch(nums, target, i, length-1);//upper bound
-        }
-
-        return index;
-        // binarySearch(nums,target,i+1,nums.length);
+       return binarySearch(nums,target,0,nums.length-1);
     }
 
-    private int binarySearch(int[] nums, int target, int low, int high) {
-        if (low > high) {
+    private int binarySearch(int[] nums, int target,int low,int high) {
+        if (low > high ) {
             return -1;
         }
         int mid = low + (high - low) / 2;
         if (nums[mid] == target) {
             return mid;
-        } else if (nums[mid] > target) {
-            return binarySearch(nums, target, low, mid - 1);
-        } else {
-            return binarySearch(nums, target, mid + 1, high);
+        }
+        //left sorted 
+         else if (nums[mid] >=nums[low]) {
+            if (target>=nums[low] && target<nums[mid]) {
+                return binarySearch(nums, target, low, mid - 1);
+            }else{
+                return binarySearch(nums, target, mid + 1, high);
+            }
+           
+        }
+        //right sorted
+         else {
+             if (target > nums[mid] && target <= nums[high]) {
+                 return binarySearch(nums, target, mid+1, high);
+             } else {
+                 return binarySearch(nums, target, low, mid-1);
+             }
+           
         }
     }
+
 
 }
