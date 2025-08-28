9205a03dfbd05322ddee633974a995a6dc5e0d89
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-08-28 15:16:21 +0530

    Time: 0 ms (100%), Space: 50.3 MB (66.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,18 +1,24 @@
 class Solution {
-     public int singleNonDuplicate(int[] nums) {
-      if (nums.length==1) {
+    public int singleNonDuplicate(int[] nums) {
+        
+       if (nums.length==1) {
         return nums[0];
-      }
-       int low = 0, high = nums.length - 2;
-       int mid=0;
+       }
+       if (nums[0] != nums[1]) {
+           return nums[0];
+       }
+       if (nums[nums.length-1]!=nums[nums.length-2]) {
+        return nums[nums.length-1];
+       }
+       int low = 1, high = nums.length - 2;
         while (low<=high) {
-             mid =low+(high-low)/2;
-        //  System.out.println("low "+ nums[low] + " mid "+ nums[mid]+ " high " + nums[high]);
-            // if (nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]) {
-            //     return nums[mid];
-            // }
+            int mid =low+(high-low)/2;
+        //  System.out.println("low "+ nums[low] + " high " + nums[high]);
+            if (nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]) {
+                return nums[mid];
+            }
            
-             if (mid%2==0) {
+            else if (mid%2==0) {
                 // if elements in even and odd  indexes are same  , then element is in  right half else in the left half 
                 if (nums[mid]==nums[mid+1]) {
                     low=mid +1;
@@ -31,7 +37,6 @@ class Solution {
                 }
             }
         }
-        // System.out.println("low " + nums[low] + " mid " + nums[mid] + " high " + nums[high]);
        return nums[low];
     }
 }
