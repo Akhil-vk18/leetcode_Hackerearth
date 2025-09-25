783b5a9e7dd6b7203d1a4347740a504b9b0acdfd
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-09-25 20:51:35 +0530

    Time: 12 ms (28.2%), Space: 47.2 MB (58.7%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -5,11 +5,12 @@ class Solution {
             low =Math.min(i, low);
             high +=i;
         }
-        System.err.println("low :"+low+"\n high :" +high);
+        int total_weight=high;
+        // System.err.println("low :"+low+"\n high :" +high);
         while (low<=high) {
             int mid=low+(high-low)/2;
-            System.err.println("low "+low);
-            if (canShip(weights, days, mid)) {// returns boolean
+            // System.err.println("low "+low);
+            if (canShip(weights, days, mid,total_weight)) {// returns boolean
                 //check for minimum
                 high=mid-1;
             } else {
@@ -19,12 +20,12 @@ class Solution {
         return low;
         
     }
-    public boolean canShip(int[] weights , int days,int capacity){
-        int total_weight=0;
-        for ( int i : weights) {
-            total_weight += i;
-        }
-        System.err.println("helper func");
+    public boolean canShip(int[] weights , int days,int capacity,int total_weight){
+        // int total_weight=0;
+        // for ( int i : weights) {
+        //     total_weight += i;
+        // }
+        // System.err.println("helper func");
         int i=1,package_no=0,delivered=0;
         while (i<=days) {
             int weight=0;
