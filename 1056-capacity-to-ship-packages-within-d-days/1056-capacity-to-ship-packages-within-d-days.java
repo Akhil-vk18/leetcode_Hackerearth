17198518c365339b3f0a808fdc6d6639076187ac
Akhil Santhosh <149648535+Akhil-vk18@users.noreply.github.com>
2025-09-25 22:13:18 +0530

    Time: 11 ms (76.74%), Space: 47.5 MB (28.8%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,16 +1,16 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low =Integer.MAX_VALUE,high =0;
+        int low =Integer.MIN_VALUE,high =0;
         for (int i : weights) {
-            low =Math.min(i, low);
+            low =Math.max(i, low);
             high +=i;
         }
-        int total_weight=high;
+
         // System.err.println("low :"+low+"\n high :" +high);
         while (low<=high) {
             int mid=low+(high-low)/2;
             // System.err.println("low "+low);
-            if (canShip(weights, days, mid,total_weight)) {// returns boolean
+            if (canShip(weights, days, mid)) {// returns boolean
                 //check for minimum
                 high=mid-1;
             } else {
@@ -20,22 +20,22 @@ class Solution {
         return low;
         
     }
-    public boolean canShip(int[] weights , int days,int capacity,int total_weight){
+    public boolean canShip(int[] weights , int days,int capacity){
         // int total_weight=0;
         // for ( int i : weights) {
         //     total_weight += i;
         // }
         // System.err.println("helper func");
-        int i=1,package_no=0,delivered=0;
-        while (i<=days) {
-            int weight=0;
-            while (package_no<weights.length&&(weight+weights[package_no])<=capacity) {
-                weight+=weights[package_no];
-                package_no++;
+      int  days_needed =1,current_weight_to_ship=0;
+        for (int weight : weights) {
+            if (current_weight_to_ship+weight>capacity) {
+                days_needed++;
+                current_weight_to_ship=weight;
             }
-            delivered += weight;i++;
+            else{
+                current_weight_to_ship+=weight;
+            }
         }
-
-        return(delivered==total_weight);
+        return(days_needed<=days);
     }
 }
